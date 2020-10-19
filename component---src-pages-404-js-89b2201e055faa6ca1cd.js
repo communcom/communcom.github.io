43958ef2a4a5461c3fdb2a38b25314a84cde97f9
Alexey Elizarov <felex-ae@ya.ru>
2020-10-19 20:03:40 +0300

    Updates

--- component---src-pages-404-js-89b2201e055faa6ca1cd.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e);var s=n("nKUr"),o=(n("q1tI"),n("Zttt")),c=n("vrFN");e.default=function(){return Object(s.jsxs)(o.a,{children:[Object(s.jsx)(c.a,{title:"404: Not found"}),Object(s.jsx)("h1",{children:"Page not Found :("}),Object(s.jsx)("p",{children:"You just hit a route that doesn't exist... the sadness."})]})}}}]);
-//# sourceMappingURL=component---src-pages-404-js-89b2201e055faa6ca1cd.js.map
