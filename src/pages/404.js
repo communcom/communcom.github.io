a10957d79e1bb1d69b8f6b52b05a8ffa257a41ba
Alexey Elizarov <felex-ae@ya.ru>
2020-10-19 21:08:02 +0300

    feat: redirect to root

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,11 @@
 import React from "react"
 
-import Layout from "../components/common/Layout"
-import SEO from "../components/seo"
+const NotFoundPage = () => {
+  if (typeof window !== "undefined") {
+    window.location = "/"
+  }
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>Page not Found :(</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  return null
+}
 
 export default NotFoundPage
